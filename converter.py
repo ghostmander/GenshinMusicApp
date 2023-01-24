e5e4c2172fc1d0a946e6b69497ea414c3b337385
Jatin Jindal <jatinjindal.2003@gmail.com>
2023-01-24 19:43:44 +0530

    encoding and stuff

---
+++ converter.py
@@ -0,0 +1,61 @@
+gapMap = {
+    1: "l",
+    2: "j",
+    3: "h",
+    4: "g",
+    6: "f",
+    8: "d",
+    12: "s",
+    16: "a"
+}
+
+
+def findGap(gapNum):
+    gapS = ""
+    if gapNum in gapMap:
+        gapS = gapMap[gapNum]
+    else:
+        keys = sorted(list(gapMap.keys()), reverse=True)
+        while gapNum > 0:
+            for key in keys:
+                if gapNum >= key:
+                    gapNum -= key
+                    gapS += gapMap[key]
+                    break
+    return gapS
+
+
+def convertABC15(song: str):
+    mapping = {
+        "C1": "15 ",
+        "C2": "16 ",
+        "C3": "17 ",
+        "C4": "18 ",
+        "C5": "19 ",
+        "B1": "8 ",
+        "B2": "9 ",
+        "B3": "10 ",
+        "B4": "11 ",
+        "B5": "12 ",
+        "A1": "1 ",
+        "A2": "2 ",
+        "A3": "3 ",
+        "A4": "4 ",
+        "A5": "5 "
+    }
+    finalSheet = ""
+    gap = 0
+    beats = song.split()
+    for i, beat in enumerate(beats):
+        if beat == ".":
+            gap += 1
+        else:
+            if gap > 0:
+                finalSheet += f"{findGap(gap)} "
+                gap = 0
+            currBeat = beat
+            for note in mapping:
+                currBeat = currBeat.replace(note, mapping[note])
+            finalSheet += f"{currBeat}"
+
+    return finalSheet
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 import ctypes
 import sys
 from pick import pick
-pydirectinput.PAUSE = 0.01
+import transcriber as tr
+pydirectinput.PAUSE = 0.0
 
 
 def inputParser(inp):
@@ -12,33 +13,26 @@ def inputParser(inp):
 
 
 def is_admin():
-    return ctypes.windll.shell32.IsUserAnAdmin() or True
+    return ctypes.windll.shell32.IsUserAnAdmin()
 
 
 if is_admin():
     # Code of your program here
-    songs = {
-        "sweaterWeather": {
-            "notes": "Q W E Q W Q E Q W Q E W E W Q W Q E W Q E H W E W Q H Q W Q H W H-H W-W-W-W-W H W-W-W-W Q W-W-W-W W Q W-W Q W E E-E-E Q Q T E E W E W E W E E E W Q H W-W-W-W-W Q-Q W-W-E-E E W . Q Q E W . Q Q W E . Q Q W W W Q Q W-W-W-W-W-W-W-W Q-Q-Q E W W W W E W Q H R E H R E Q W Q W Q W E W Q H R E H R E W W Q J J H J Q",
-            "bpm": 180,
-            "repeatDelay": 0.1,
-            "songName": "Sweater Weather - The Neighbourhood"
-        }
-    }
+    songs = tr.songSheet
 
     option, index = pick(list(songs.keys()), "Select a song",
                          indicator="->", default_index=0)
 
-    print(f"Playing {songs[option]['songName']} in 2 seconds...")
+    print(f"Playing {songs[option]['Name']} in 2 seconds...")
     time.sleep(2)
 
     song = songs[option]
-    bpm = song["bpm"]
-    for keys in inputParser(song["notes"]):
-        for key in keys:
-            pydirectinput.press(key.lower())
-            time.sleep(song["repeatDelay"])
-        time.sleep((len(keys) == 1) * 60/bpm)
+    bpm = song["BPM"]
+    for i in tr.songParser(song["notes"]):
+        if isinstance(i, int):
+            time.sleep(i/16 * bpm/240)
+            continue
+        pydirectinput.press(i)
 
 else:
     # Re-run the program with admin rights
