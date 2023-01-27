dc31408a4aba59ce5d9843fa100dba4e964fc6a3
Jatin Jindal <jatinjindal.2003@gmail.com>
2023-01-27 23:58:38 +0530

    Almost done with UI

--- ui/main.py
+++ ui/main.py
@@ -1,10 +1,11 @@
 import sys
+import time
 import pyautogui
 import pydirectinput
 from src.songs import songSheet
 from src.main import play_song
 from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QComboBox, QPushButton, QApplication, QMainWindow, QLabel, QSpacerItem, QSizePolicy as QSize
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QComboBox, QPushButton, QApplication, QMainWindow, QLabel, QSpacerItem, QSizePolicy as QSize, QSlider
 from PyQt5.QtGui import QResizeEvent, QMouseEvent
 
 
@@ -20,12 +21,16 @@ class Main(QMainWindow):
         self.layout = QVBoxLayout()
         self.main_layout = QVBoxLayout()
         self.songpicker_layout = QHBoxLayout()
+        self.delay_layout = QVBoxLayout()
+        self.song_config_layout = QVBoxLayout()
         self.infolabel_layout = QVBoxLayout()
 
         # * Song Picker
         self.init_songpicker()
         # * Button
         self.init_play_button()
+        # * Delay Slider
+        self.init_delay_slider()
         # * Info Label
         self.init_info_label()
 
@@ -35,18 +40,23 @@ class Main(QMainWindow):
         # * Layout Config
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
-        self.songpicker_layout.setContentsMargins(50, 0, 50, 0)
+        # self.songpicker_layout.setContentsMargins(50, 0, 50, 0)
+        self.song_config_layout.setContentsMargins(50, 0, 50, 0)
+        self.delay_layout.setContentsMargins(0, 20, 0, 70)
         self.infolabel_layout.setContentsMargins(50, 0, 50, 0)
 
         # * Add Widgets to Layouts
         self.layout.addWidget(MyBar(self))
         self.layout.addLayout(self.main_layout)
-        self.main_layout.addLayout(self.songpicker_layout)
+        self.main_layout.addLayout(self.song_config_layout)
+        self.song_config_layout.addLayout(self.songpicker_layout)
+        self.song_config_layout.addLayout(self.delay_layout)
+        # self.song_config_layout.addWidget(self.delay_slider)
         self.main_layout.addLayout(self.infolabel_layout)
         self.main_layout.addItem(spacer)
 
         # * Window Settings
-        self.setMinimumSize(800, 400)
+        self.setMinimumSize(800, 600)
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setWindowTitle("Genshin Music App")
         self.setCentralWidget(widget)
@@ -87,9 +97,9 @@ class Main(QMainWindow):
                     border-top-right-radius: 9px;
                     border-bottom-right-radius: 9px;
                 }
-                
+
                 QComboBox::down-arrow { image: url(ui/assets/dropdown.png); }
-                
+
                 QComboBox QAbstractItemView {
                     border: 1px solid darkgray;
                     outline: 0;
@@ -115,21 +125,82 @@ class Main(QMainWindow):
         self.play_button.setCursor(Qt.PointingHandCursor)
 
     def init_info_label(self):
-        self.info_label = QLabel("This is a test")
-        self.info_label.setStyleSheet("font-size: 20px;")
+        self.info_label = QLabel("")
+        self.info_label.setWordWrap(True)
         self.infolabel_layout.addWidget(self.info_label)
 
     def play_button_clicked(self):
         song = songSheet[self.songPicker.currentText()]
-        self.playing = True
         self.play_button.setDisabled(True)
         try:
+            print(song)
+            self.info_label.setStyleSheet("font-size: 30px; color: white;")
+            self.info_label.setAlignment(Qt.AlignLeft)
+            for i in range(self.delay_slider.value(), 0, -1):
+                self.info_label.setText(f"Starting in {i}s...")
+                QApplication.processEvents()
+                time.sleep(1)
+            self.info_label.setText(
+                f"Now Playing:\t{song['Name']}\n"
+                # f"Notes:\t\t{song['notes']}\n"
+                f"\nTo stop the song, move your mouse to the top left corner of your screen\n"
+            )
+            # ! Dont know why but this is needed to make the label update
+            for _ in range(10):
+                QApplication.processEvents()
             play_song(song)
-            self.playing = False
             self.play_button.setDisabled(False)
-            print("Finished playing song")
+            self.info_label.setText(f"Song Finished!!")
+            color = "#00FFFF"
         except pydirectinput.FailSafeException:
-            print("Stopping due to FailSafeException")
+            self.info_label.setText(f"Song Stopped by mouse movement.")
+            color = "#FF3030"
+        except Exception as e:
+            self.info_label.setText(f"Error: {e}")
+            color = "#FF3030"
+        finally:
+            self.info_label.setStyleSheet(f"font-size: 40px; color: {color};")
+            self.info_label.setAlignment(Qt.AlignCenter)
+            self.play_button.setDisabled(False)
+
+    def init_delay_slider(self):
+        self.delay_slider = QSlider(Qt.Horizontal)
+        self.delay_slider.setRange(0, 10)
+        self.delay_slider.setValue(2)
+        self.delay_slider.setTickInterval(1)
+        self.delay_slider.setTickPosition(QSlider.TicksBelow)
+        self.delay_slider.setSingleStep(1)
+        # Label for the slider
+        self.delay_label = QLabel("Delay: 2s")
+        self.delay_label.setStyleSheet("font-size: 20px; color: white;")
+        self.delay_layout.addWidget(self.delay_label)
+        self.delay_slider.valueChanged.connect(
+            lambda: self.delay_label.setText(f"Delay: {self.delay_slider.value()}s"))
+        # Styling the slider
+        self.delay_slider.setStyleSheet("""
+            QSlider::groove:horizontal {
+                border: 1px solid #999999;
+                height: 8px;
+                background: #181818;
+                margin: 2px 0;
+            }
+            QSlider::handle:horizontal {
+                background: #77e;
+                border: 1px solid #5c5c5c;
+                width: 18px;
+                margin: -2px 0;
+                border-radius: 3px;
+            }
+            QSlider::handle:horizontal:hover {
+                background: #99f;
+                border: 1px solid #5c5c5c;
+                border-radius: 3px;
+                width: 18px;
+                margin: -2px 0;
+            }
+        """)
+        self.delay_slider.setCursor(Qt.PointingHandCursor)
+        self.delay_layout.addWidget(self.delay_slider)
 
 
 class MyBar(QWidget):
